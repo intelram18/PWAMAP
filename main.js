17e1705381226ddeb6a28405977847154a1d4f3f
intelram18 <65332133+intelram18@users.noreply.github.com>
2020-05-16 09:49:26 +0800

    Update main.js

--- main.js
+++ main.js
@@ -67,8 +67,7 @@ function notification() {
 // Code to handle install prompt on desktop
 
 let deferredPrompt;
-const addBtn = document.querySelector('.add-button');
-addBtn.style.display = 'none';
+
 
 window.addEventListener('beforeinstallprompt', (e) => {
   // Prevent Chrome 67 and earlier from automatically showing the prompt
@@ -76,11 +75,11 @@ window.addEventListener('beforeinstallprompt', (e) => {
   // Stash the event so it can be triggered later.
   deferredPrompt = e;
   // Update UI to notify the user they can add to home screen
-  addBtn.style.display = 'block';
+
 
   addBtn.addEventListener('click', (e) => {
     // hide our user interface that shows our A2HS button
-    addBtn.style.display = 'none';
+    
     // Show the prompt
     deferredPrompt.prompt();
     // Wait for the user to respond to the prompt
